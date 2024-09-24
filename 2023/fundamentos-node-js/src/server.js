7f29677c20bc534540ef111299c62af5b29fe856
Leonardo Anthony <leonardoanthony.dev@gmail.com>
2024-09-24 17:50:06 -0300

    Streams

--- 2023/fundamentos-node-js/src/server.js
+++ 2023/fundamentos-node-js/src/server.js
@@ -1,7 +1,34 @@
 import http from 'node:http';
 
+const users = [];
+
+let nextId = 1;
+
 const server = http.createServer((req, res) => {
-    return res.end('Hello World');
+
+    const {method, url} = req;
+
+    if(method == 'GET' && url == '/users'){
+        return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users));
+    }
+    
+    if(method == 'POST' && url == '/users'){
+
+        users.push({
+            id: nextId,
+            name: 'Jhon Doe',
+            email: 'jhondoe@example.com'
+        })
+
+        nextId++;
+
+        return res.writeHead(201).end();
+    }
+
+
+    return res.writeHead(404).end();
 });
 
 
